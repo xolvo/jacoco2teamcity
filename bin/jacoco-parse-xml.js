7f0d7cee18f5fed22579649b35cd67c2ef1f40d5
Sveinung Kval Bakken <sveinung.bakken@gmail.com>
2015-07-29 12:18:45 -0400

    Got rid of abs prefix for percentage

--- bin/jacoco-parse-xml.js
+++ bin/jacoco-parse-xml.js
@@ -29,21 +29,21 @@ fs.readFile(report_path, function(err, data) {
                     console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsLCovered' value='%s']", item.covered);
                     var total = sumStrings(item.covered, item.missed);
                     console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsLTotal' value='%s']", total);
-                    console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsLPercentage' value='%s']", item.covered * 1.0 / total);
+                    console.log("##teamcity[buildStatisticValue key='CodeCoverageLPercentage' value='%s']", item.covered * 1.0 / total);
                 break;
 
                 case 'METHOD':
                     console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsMCovered' value='%s']", item.covered);
                     var total = sumStrings(item.covered, item.missed);
                     console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsMTotal' value='%s']", total);
-                    console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsMPercentage' value='%s']", item.covered * 1.0 / total);
+                    console.log("##teamcity[buildStatisticValue key='CodeCoverageMPercentage' value='%s']", item.covered * 1.0 / total);
                 break;
 
                 case 'CLASS':
                     console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsCCovered' value='%s']", item.covered);
                     var total = sumStrings(item.covered, item.missed);
                     console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsCTotal' value='%s']", total);
-                    console.log("##teamcity[buildStatisticValue key='CodeCoverageAbsMPercentage' value='%s']", item.covered * 1.0 / total);
+                    console.log("##teamcity[buildStatisticValue key='CodeCoverageCPercentage' value='%s']", item.covered * 1.0 / total);
                 break;
             }
         }
